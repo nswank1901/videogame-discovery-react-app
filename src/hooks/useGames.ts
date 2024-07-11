e741b4f0ea5cf882164bae50e434116d4e2b8cac
nswank1901 <93402347+nswank1901@users.noreply.github.com>
2024-07-11 14:52:23 -0400

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platform }[]
   }
   
   interface FetchGamesResponse {
@@ -14,7 +21,7 @@ export interface Game {
   }
 
 const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
+  const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
